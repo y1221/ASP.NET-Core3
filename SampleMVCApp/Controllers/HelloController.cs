fa3ac0974fd6ef281461c1c0a3316218bb0c7c54
okayu <yuto.gio.0515@gmail.com>
2024-05-31 21:07:30 +0900

    2.2 コントローラーとビューの基本

---
+++ SampleMVCApp/Controllers/HelloController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SampleMVCApp.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["message"] = "Hello World!";
+            return View();
+        }
+    }
+}
